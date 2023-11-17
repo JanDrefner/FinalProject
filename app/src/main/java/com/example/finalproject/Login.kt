e63e79c063846488129687f33f75e0caed3d380f
JanDrefner <jandrefner.santos.cics@ust.edu.ph>
2023-11-17 13:01:12 +0800

    Changed Colors for Login Page

--- app/src/main/java/com/example/finalproject/Login.kt
+++ app/src/main/java/com/example/finalproject/Login.kt
@@ -2,6 +2,7 @@ package com.example.finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.android.material.textfield.TextInputEditText
 
 class Login : AppCompatActivity() {
@@ -10,5 +11,9 @@ class Login : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         var edtEmail : TextInputEditText = findViewById(R.id.edtLoginEmail)
+        var edtPassword : TextInputEditText = findViewById(R.id.edtPass)
+
+        var btnLogin : Button = findViewById(R.id.btnLogin)
+        var btnSignup : Button = findViewById(R.id.btnSignup)
     }
 }
