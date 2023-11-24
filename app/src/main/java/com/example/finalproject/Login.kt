fbf68c122d0d0bd99988cc6873b90a2778bc916d
louisivannn <louisivan.virgo.cics@ust.edu.ph>
2023-11-24 13:41:59 +0800

    changes to login.kt

--- app/src/main/java/com/example/finalproject/Login.kt
+++ app/src/main/java/com/example/finalproject/Login.kt
@@ -2,18 +2,59 @@ package com.example.finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.view.View
 import android.widget.Button
 import com.google.android.material.textfield.TextInputEditText
+import java.security.interfaces.EdECKey
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        var edtEmail : TextInputEditText = findViewById(R.id.edtLoginEmail)
-        var edtPassword : TextInputEditText = findViewById(R.id.edtPass)
+        var edtEmail: TextInputEditText = findViewById(R.id.edtLoginEmail)
+        var edtPassword: TextInputEditText = findViewById(R.id.edtPass)
 
-        var btnLogin : Button = findViewById(R.id.btnLogin)
-        var btnSignup : Button = findViewById(R.id.btnSignup)
+        var btnLogin: Button = findViewById(R.id.btnLogin)
+        var btnSignup: Button = findViewById(R.id.btnSignup)
+        // val allDecks : ArrayList<Deck> = ArrayList<Deck>()
+
+
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString().trim()
+            val pss = edtPassword.text.toString()
+
+
+            when {
+                email.isEmpty() -> {
+                    edtEmail.error = "Please enter an email."
+                    edtEmail.requestFocus()
+                    return@setOnClickListener
+                }
+
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                    edtEmail.error = "Enter a valid email."
+                    edtEmail.requestFocus()
+                    return@setOnClickListener
+                }
+
+                pss.isEmpty() -> {
+                    edtPassword.error = "Please enter a password."
+                    edtPassword.requestFocus()
+                    return@setOnClickListener
+                }
+
+                pss.length < 6 -> {
+                    edtPassword.error = "Invalid password."
+                    edtPassword.requestFocus()
+                    return@setOnClickListener
+                }
+            }
+
+            // Continue with the rest of your code if all validation passes
+        }
+
+
     }
 }
--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
