ce7971ba2fdb3e88c0521342ef195d77ac71ee32
louisivannn <louisivan.virgo.cics@ust.edu.ph>
2023-12-07 11:58:24 +0800

    added layouts

---
+++ app/src/main/java/com/example/finalproject/create_deck_popup.kt
@@ -0,0 +1,11 @@
+package com.example.finalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class create_deck_popup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_deck_popup)
+    }
+}
